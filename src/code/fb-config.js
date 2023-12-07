6257c5e5453d689cfd2bafb0806d135c80474e3b
Moumy Ndiaye <e2177120@cmaisonneuve.qc.ca>
2023-12-07 14:00:46 -0500

    fb config changement

---
+++ src/code/fb-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDG4Lw0D7hHVfBn9FiOEoOrEH6_vAobcCk",
+  authDomain: "tp2-h23-idj.firebaseapp.com",
+  projectId: "tp2-h23-idj",
+  storageBucket: "tp2-h23-idj.appspot.com",
+  messagingSenderId: "535460037483",
+  appId: "1:535460037483:web:181b6c552b0f3d8c83c22e"
+};
+export default firebaseConfig;
